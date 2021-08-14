92168b5818f15859829b053a08a99a633deb6960
Carlosgg <carlosgilglez@gmail.com>
2021-08-15 00:18:39 +0100

    nex examples

--- examples/pyqt-gui/pyqt_pyboard_servo_key_controller.py
+++ examples/pyqt-gui/pyqt_pyboard_servo_key_controller.py
@@ -5,27 +5,18 @@ from PyQt5.QtCore import Qt
 from upydevice import Device
 
 
-class Slider(QWidget):
+class KeyController(QWidget):
     def __init__(self, parent=None, device=None):
-        super(Slider, self).__init__(parent)
+        super(KeyController, self).__init__(parent)
 
         layout = QHBoxLayout()
-        self.l1 = QLabel("Angle")
+        self.l1 = QLabel("Press UP,DOWN keys to move servo angle: ")
         self.l1.setAlignment(Qt.AlignCenter)
         self.label = QLabel("0", self)
         self.label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
         self.label.setMinimumWidth(80)
+        self.angle = 0
 
-        self.sl = QSlider(Qt.Horizontal, self)
-        self.sl.setRange(-30, 90)
-        self.sl.setPageStep(5)
-        self.sl.setValue(10)
-        self.sl.setTickPosition(QSlider.TicksBelow)
-        self.sl.setTickInterval(10)
-        self.sl.valueChanged.connect(self.updateLabel)
-        self.sl.sliderReleased.connect(self.valuechange)
-
-        layout.addWidget(self.sl)
         layout.addSpacing(15)
         layout.addWidget(self.l1)
         layout.addWidget(self.label)
@@ -34,14 +25,20 @@ class Slider(QWidget):
 
         self.device = device
 
-    def valuechange(self):
-        angle = self.sl.value()
-        self.device.wr_cmd("s1.angle({}, 1000)".format(angle))
-        print(f"Setting angle to {angle}")
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Up:
+                if self.angle < 90:
+                    self.angle += 4
+                    self.device.wr_cmd("s1.angle({}, 100)".format(self.angle))
+                    print(f"Setting angle to {self.angle}")
+                    self.label.setText(str(self.angle))
 
-    def updateLabel(self, value):
-
-        self.label.setText(str(value))
+        elif e.key() == Qt.Key_Down:
+                if self.angle > -90:
+                    self.angle -= 4
+                    self.device.wr_cmd("s1.angle({}, 100)".format(self.angle))
+                    print(f"Setting angle to {self.angle}")
+                    self.label.setText(str(self.angle))
 
 
 def main():
@@ -58,14 +55,11 @@ def main():
     print("Connected")
 
     app = QApplication(sys.argv)
-    widget = QWidget()
 
-    angle_slider = Slider(parent=widget, device=mydev)
-    # angle_slider.show()
+    Keycontroller = KeyController(device=mydev)
 
-    # widget.setGeometry(300,300,350,250)
-    widget.setWindowTitle("Pyboard Servo slider")
-    widget.show()
+    Keycontroller.show()
+
     sys.exit(app.exec_())
 
 
--- examples/pyqt-gui/pyqt_toggle_led.py
+++ examples/pyqt-gui/pyqt_toggle_led.py
@@ -1,39 +1,85 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+import os
 from upydevice import Device
+
+ICON_RED_LED_OFF = os.path.join(os.getcwd(), "icons/xsled-red-off.png")
+ICON_RED_LED_ON = os.path.join(os.getcwd(), "icons/xsled-red-on.png")
+
+
+class LED_Label(QLabel):
+    def __init__(self, parent=None):
+        super(LED_Label, self).__init__(parent)
+        self._value = 0
+        self.off = QPixmap(ICON_RED_LED_OFF)  #.scaled(64, 64, Qt.KeepAspectRatio)
+        self.on = QPixmap(ICON_RED_LED_ON)  #.scaled(64, 64, Qt.KeepAspectRatio)
+        self.setPixmap(self.off)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        self._value = val
+        if val:
+            self.setPixmap(self.on)
+            print('on')
+        else:
+            self.setPixmap(self.off)
+            print('off')
+        self.repaint()
+
+
+    def toggle(self):
+        self.value = not self._value
+
+
+class LED_Widget(QWidget):
+    def __init__(self, parent=None, dev=None):
+        super(LED_Widget, self).__init__(parent)
+        self.button = QPushButton(self)
+        self.button.setText("Toggle Led")
+        self.button.move(100, 100)
+        self.dev = dev
+        self.led = LED_Label(self)
+        self.led.setScaledContents(True)
+        self.led.move(140, 50)
+        self.button.clicked.connect(self.toggle_led)
+
+
+
+    def toggle_led(self):
+        self.dev.wr_cmd("led.value(not led.value())")
+        self.led.toggle()
+
 
 
 
 def main():
 
     # SerialDevice
-    print('Connecting to device...')
-    mydev = Device('/dev/tty.SLAB_USBtoUART', init=True)
+    print("Connecting to device...")
+    mydev = Device("/dev/tty.SLAB_USBtoUART", init=True)
 
     # # WebSocketDevice
     # mydev = Device('192.168.1.73', 'keyespw', init=True)
 
     # # BleDevice
     # mydev = Device('9998175F-9A91-4CA2-B5EA-482AFC3453B9', init=True)
-    print('Connected')
-    def toggle_led():
-        mydev.wr_cmd('led.value(not led.value())')
+    print("Connected")
+
 
     app = QApplication(sys.argv)
-    widget = QWidget()
+    led_widget = LED_Widget(dev=mydev)
+    led_widget.setGeometry(50, 50, 320, 200)
+    led_widget.setWindowTitle("Upydevice Button Led Toggle")
+    led_widget.show()
 
-    button1 = QPushButton(widget)
-    button1.setText("Toggle Led")
-    button1.move(100,100)
-    button1.clicked.connect(toggle_led)
-
-
-    widget.setGeometry(50,50,320,200)
-    widget.setWindowTitle("Upydevice Button Led Toggle")
-    widget.show()
-    sys.exit(app.exec_())
+    QApplication.instance().exec_()
 
 
-
-if __name__ == '__main__':
-   main()
+if __name__ == "__main__":
+    main()
